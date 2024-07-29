35615e487a822c8239e9d5f9b8368b46783d1862
Divakar P S <divakar.p.s@oracle.com>
2024-07-29 20:52:39 +0530

    added sample producer consumer queue for a sampleJob

--- dist/index.js
+++ dist/index.js
@@ -6,8 +6,15 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const serverConfig_1 = __importDefault(require("./config/serverConfig"));
 const routes_1 = __importDefault(require("./routes"));
+const sampleQueueProducer_1 = __importDefault(require("./producers/sampleQueueProducer"));
+const SampleWorker_1 = __importDefault(require("./worker/SampleWorker"));
 const app = (0, express_1.default)();
 app.use("/api", routes_1.default);
 app.listen(serverConfig_1.default.PORT, () => {
     console.log(`Server is running on port ${serverConfig_1.default.PORT}`);
+    (0, sampleQueueProducer_1.default)("SampleJob", {
+        name: "divps",
+        company: "Oracle"
+    });
+    (0, SampleWorker_1.default)("SampleQueue");
 });
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express, { Express } from "express";
 import serverConfig from "./config/serverConfig";
 import apiRouter from "./routes";
+import sampleQueueProducer from "./producers/sampleQueueProducer";
+import SampleWorker from "./worker/SampleWorker";
 
 const app: Express = express();
 
@@ -9,4 +11,12 @@ app.use("/api", apiRouter);
 
 app.listen(serverConfig.PORT, () => {
   console.log(`Server is running on port ${serverConfig.PORT}`)
+
+  sampleQueueProducer("SampleJob", {
+    name: "divps",
+    company: "Oracle"
+  });
+
+  SampleWorker("SampleQueue");
+
 })
